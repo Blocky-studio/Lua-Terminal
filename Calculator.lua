38cee44429e80f6cf81c3fe3b060ef1e172dd358
Blocklandcrpto <theconsoleguy101@gmail.com>
2020-11-19 16:14:12 +0000

    update

--- Calculator.lua
+++ Calculator.lua
@@ -1,11 +1,32 @@
-    
+    Calcadd =[[
+      if op == "+"then
+        io.write("whats the first number?\n")
+        ADD1 = io.read()
+        io.write("whats the second number?\n")
+        ADD2 = io.read()
+        io.write("= ")
+        AE = ADD1+ADD2
+        io.write(AE)
+        io.write(" \n")
+        OPT = io.read()
+        if OPT == "exit" then 
+        dofile("main.lua")
+        elseif OPT = "+"
+
+        end
+    ]]
+
+Calcadd = loadstring(Calcadd)
+
+
     io.write("Calculator Terminal Program v1.0\n")
     io.write("what operation?\n")
     op = io.read()
     
     if op == "exit"then
-        io.write("Exiting")
-        
+        io.write("Exiting\n")
+        dofile("main.lua")
+
         
         end
         
@@ -14,6 +35,15 @@
         ADD1 = io.read()
         io.write("whats the second number?\n")
         ADD2 = io.read()
-        io.write("=\n")
-        io.write(ADD1+ADD2)
+        io.write("= ")
+        AE = ADD1+ADD2
+        io.write(AE)
+        io.write(" \n")
+        OPT = io.read()
+        if OPT == "exit" then 
+        dofile("main.lua")
+        elseif OPT == "+" then
+        Calcadd();
+
+        end
         end
--- main.lua
+++ main.lua
@@ -10,16 +10,21 @@ if cmd == "" then
     end
 if cmd == "cal"then
     Calculator()
-    end    
+    end  
 ]]
 
 home = loadstring(home)
-home()
 
+
+local Calculator = assert(loadfile("Calculator.lua"))
+io.write("Terminal is starting up --- done!\n")
+io.write("Making sure everything works --- Done!\n")
+cmd = io.read()
+io.write(">")
 if cmd == "" then 
-    io.write("Please type a command/n") 
+    io.write("Please type a command\n") 
     home()
     end
 if cmd == "cal"then
     Calculator()
-    end    
+    end  
