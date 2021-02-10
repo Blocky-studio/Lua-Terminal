d69c324e636193d7a1e59bac59bfa6fbc027659c
Blocklandcrpto <herbgamerlol@gmail.com>
2021-02-10 15:59:48 +0000

    v4.1 added new python GUI!

--- pythonlib/ui/gui.py
+++ pythonlib/ui/gui.py
@@ -1,21 +1,36 @@
-import PySimpleGUI as sg
-import os
+import PySimpleGUI as sg   
+import os   
 
-layout = [[sg.Text("Luthon")], [sg.Button("Logout")], [sg.Button("Terminal")]]
+sg.ChangeLookAndFeel('LightGreen')      
+sg.SetOptions(element_padding=(0, 0))      
 
-# Create the window
-window = sg.Window("menu", layout)
+    # ------ Menu Definition ------ #      
+menu_def = [['Menu', ['Logout', 'imager', 'Terminal'  ]],      
+                ['Edit', ['Paste', ['Special', 'Normal', ], 'Undo', 'save', 'open'], ],      
+                ['Help', 'About...'], ]      
 
-# Create an event loop
-while True:
-    event, values = window.read()
-    # End program if user closes window or
-    # presses the OK button
-    if event == "Logout":
-     os.system("lua lualib/utils/secure/login.lua")
-    elif event == "Terminal":
-     os.system("lua ui.lua")
-    elif event == sg.WIN_CLOSED:
-     break
-    
-window.close()
+    # ------ GUI Defintion ------ #      
+layout = [      
+        [sg.Menu(menu_def, )],      
+        [sg.Output(size=(60, 20))]      
+             ]      
+
+window = sg.Window("Home", layout, default_element_size=(12, 1), auto_size_text=False, auto_size_buttons=False,      
+                       default_button_element_size=(12, 1))      
+
+    # ------ Loop & Process button menu choices ------ #      
+while True:      
+        event, values = window.read()      
+        if event == sg.WIN_CLOSED or event == 'Logout':      
+            os.system("lualib/utils/logoff.lua")    
+        print('Button = ', event)      
+        # ------ Process menu choices ------ #      
+        if event == 'Terminal':      
+            os.system("lua ui.lua")    
+        elif event == 'open':      
+            filename = sg.popup_get_file('file to open', no_window=True)      
+            print(filename)
+        elif event == 'imager':
+            os.system("python pythonlib/ui/img.py") 
+        elif event == 'About...':
+          sg.popup('Luthon v4.0, ui v1.0')     
